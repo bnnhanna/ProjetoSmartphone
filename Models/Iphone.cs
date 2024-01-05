38583fb044198088a47e53f4daae75cb879cdf16
Hanna <hbnfl33@gmail.com>
2024-01-05 00:50:09 -0300

    Mensagem do commit

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} na App Store do {Modelo}.");
+        }
+
+        public void ImprimirInformacoes()
+        {
+            Console.WriteLine($"Smartphone: {Modelo}");
+            Console.WriteLine($"Recebendo ligação...");
+            Console.WriteLine($"Instalando o Twitter");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main()
+        {
+            // SmartPhone Nokia:
+            Nokia nokia = new Nokia("123456", "Nokia 3310", "123ABC", 16);
+            Console.WriteLine($"Nokia");
+            nokia.Ligar();
+            nokia.InstalarAplicativo("JogoDaCobrinha");
+
+            Console.WriteLine(); 
+
+            // Smartphone Iphone:
+            Iphone iphone = new Iphone("789012", "iPhone 12", "456DEF", 64);
+            Console.WriteLine($"Iphone:");
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Twitter");
+        }
+    }
+}
