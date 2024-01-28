03994c364a37b19f3dc51362ee0ebbb434976321
Hanna Lima <155580108+bnnhanna@users.noreply.github.com>
2024-01-28 16:50:59 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,13 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+         
         private string Numero { get; set; }
         private string Modelo { get; set; }
         public string IMEI { get; set; }
         public int Memoria { get; set; }
        
-        // TODO: Passar os parâmetros do construtor para as propriedades
+      
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
